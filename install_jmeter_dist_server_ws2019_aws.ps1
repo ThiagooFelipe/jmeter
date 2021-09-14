07601634822e254bc4e9494a76f24ec945da67df
Jean Macena <34484104+jeancbezerra@users.noreply.github.com>
2021-09-14 17:43:00 -0300

    A

--- install_jmeter_dist_server_ws2019_aws.ps1
+++ install_jmeter_dist_server_ws2019_aws.ps1
@@ -7,13 +7,25 @@
 # Exec: wget https://raw.githubusercontent.com/jeancbezerra/jmeter/master/install_jmeter_dist_server_ws2019_aws.ps1 -OutFile install_jmeter_dist_server_ws2019_aws.ps1
 # Exec: ./install_jmeter_dist_server_ws2019_aws.ps1
 
-#Get-ChildItem -Path C:\YAMAN\apache-jmeter-5.4.1.zip -File -Recurse | Remove-Item -Verbose
-#Remove-Item -Path C:\YAMAN\apache-jmeter-5.4.1\ -Force
+New-Item -Path C:\BOTS\OUTPUTS\ -ItemType directory -Force
+New-Item -Path C:\yaman-server\ -ItemType directory -Force
 
-New-Item -Path C:\YAMAN\ -ItemType directory -Force
-cd C:\YAMAN\
+cd C:\yaman-server\
 
-wget https://yaman-apm-static-files.s3.sa-east-1.amazonaws.com/apache-jmeter-5.4.1-server.zip -OutFile apache-jmeter-5.4.1-server.zip
-#wget https://yaman-apm-static-files.s3.sa-east-1.amazonaws.com/apache-jmeter-5.4.1-server.zip -OutFile apache-jmeter-5.4.1-server-master.zip
+wget https://yaman-apm-static-files.s3.sa-east-1.amazonaws.com/apache-jmeter-5.4.1.zip -OutFile C:\yaman-server\apache-jmeter-5.4.1.zip
+wget https://yaman-apm-static-files.s3.sa-east-1.amazonaws.com/apache-jmeter-5.4.1.zip -OutFile C:\yaman-server\apache-jmeter-5.4.1.zip
+Expand-Archive -Path C:\yaman-server\apache-jmeter-5.4.1.zip -DestinationPath C:\yaman-server\
 
-Expand-Archive -Path C:\YAMAN\apache-jmeter-5.4.1-server.zip -DestinationPath C:\YAMAN\
+$Env:JAVA_HOME='C:\Program Files\Eclipse Foundation\jdk-11.0.12.7-hotspot'
+$Env:JMETER_HOME='C:\yaman-server\apache-jmeter-5.4.1'
+
+[Environment]::SetEnvironmentVariable("JAVA_HOME","C:\Program Files\Eclipse Foundation\jdk-11.0.12.7-hotspot","Machine")
+[Environment]::SetEnvironmentVariable("JMETER_HOME","C:\YAMAN\apache-jmeter-5.4.1","Machine")
+
+$path = [Environment]::GetEnvironmentVariable('Path', 'Machine')
+$newpath = $path + ";" + $Env:JAVA_HOME + "\bin" + ";" + $Env:JMETER_HOME + "\bin"
+[Environment]::SetEnvironmentVariable("Path", $newpath, 'Machine')
+
+
+Set-NetFirewallProfile -All -Enabled True
+New-NetFirewallRule -DisplayName 'JMETER-PORT-INBOUND' -Profile @('Domain','Private','Public') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('4000')
