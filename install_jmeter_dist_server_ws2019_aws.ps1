76ec42e7548d4fd25d4a152399e83f2f44fc1df3
Jean Macena <34484104+jeancbezerra@users.noreply.github.com>
2021-09-14 16:53:44 -0300

    A

--- install_jmeter_dist_server_ws2019_aws.ps1
+++ install_jmeter_dist_server_ws2019_aws.ps1
@@ -7,11 +7,11 @@
 # Exec: wget https://raw.githubusercontent.com/jeancbezerra/jmeter/master/install_jmeter_dist_server_ws2019_aws.ps1 -OutFile install_jmeter_dist_server_ws2019_aws.ps1
 # Exec: ./install_jmeter_dist_server_ws2019_aws.ps1
 
-New-Item -Path C:\YAMAN\ -ItemType directory -Force
-cd C:\YAMAN\
-
 Get-ChildItem -Path C:\YAMAN\apache-jmeter-5.4.1.zip -File -Recurse | Remove-Item -Verbose
 Remove-Item -Path C:\YAMAN\apache-jmeter-5.4.1\ -Force
+
+New-Item -Path C:\YAMAN\ -ItemType directory -Force
+cd C:\YAMAN\
 
 wget https://yaman-apm-static-files.s3.sa-east-1.amazonaws.com/apache-jmeter-5.4.1-server.zip -OutFile apache-jmeter-5.4.1-server.zip
 #wget https://yaman-apm-static-files.s3.sa-east-1.amazonaws.com/apache-jmeter-5.4.1-server.zip -OutFile apache-jmeter-5.4.1-server-master.zip
